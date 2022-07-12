3cb384a5fb4d9dc57bbfec7a717aed9b37d7c99f
Brian Wangora <brianwangora@gmail.com>
2022-07-12 13:11:44 +0300

    class ended

--- src/Page.js
+++ src/Page.js
@@ -11,15 +11,20 @@ const features = [
     'Platform Independent.',
 ]
 
+// jsx
+const JSXItem = <input type={'submit'} className={'button'}/>
+const OtherJSX = <div>{JSXItem}</div>
+
 function Homepage(){
     return (
         <div>
             <h4>Title</h4>
-            {/* <h1>{title}</h1> */}
+            <h1>{title}</h1>
             <h4>Image</h4>
-            {/* <img src={image}/> */}
+            <img src={image}/>
             <h4>Features</h4>
-            {/* {features.map((element) => <p>{element}</p>)} */}
+            {/* {features.forEach((element) => <p>{element}</p>)} */}
+            {features.map((element) => <p>{element}</p>)}
         </div>
     )
 }
